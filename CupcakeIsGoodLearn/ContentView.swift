b7c185def81902b31301b641ebdf7c7449562001
Bagus Triyanto <44406611+bagusisgood@users.noreply.github.com>
2020-07-04 16:04:04 +0700

    Added 1.1: - Navigation View - List View - URLSession - Get data from iTunes API - Format List using SongView

--- CupcakeIsGoodLearn/ContentView.swift
+++ CupcakeIsGoodLearn/ContentView.swift
@@ -29,10 +29,70 @@ class User: ObservableObject, Codable {
     }
 }
 
-struct ContentView: View {
+struct Response: Codable {
+    var results: [Result]
+}
+
+struct Result: Codable {
+    var trackId: Int
+    var trackName: String
+    var collectionName: String
+}
+
+struct SongView: View {
+    let title: String
+    let album: String
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack(alignment:.center) {
+            Text(title)
+                .font(.system(size: 22))
+                .bold()
+                .multilineTextAlignment(.center)
+            Spacer()
+            Text(album)
+                .font(.headline)
+        }
+        .padding()
+        .frame(width: 350, height: 120)
+        .background(LinearGradient(gradient: Gradient(colors: [.purple, .blue]), startPoint: .topLeading, endPoint: .bottomTrailing))
+        .cornerRadius(15)
+        .foregroundColor(.white)
+    }
+}
+
+struct ContentView: View {
+    @State private var results = [Result]()
+    
+    var body: some View {
+        NavigationView {
+            List(results, id:\.trackId) { item in
+                SongView(title: item.trackName, album: item.collectionName)
+            }
+            .onAppear(perform: loadData)
+            .navigationBarTitle(Text("Swifty Good"))
+        }
+    }
+    
+    func loadData() {
+        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
+            print("Invalid URL")
+            return
+        }
         
+        let request = URLRequest(url: url)
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let data = data {
+                if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
+                    DispatchQueue.main.async {
+                        self.results = decodedResponse.results
+                    }
+                    return
+                }
+            }
+            print("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
+        }.resume()
     }
 }
 
